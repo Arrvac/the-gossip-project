b8c82878c19742b4982a55099fd7dad75fa7b931
mmolinar <mariamolinaryarza@gmail.com>
2020-10-27 17:58:07 +0100

    Create a form partial which renders in gossips new and edit

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -13,7 +13,7 @@ class GossipsController < ApplicationController
   end
 
   def create
-    @gossip = Gossip.new(params.require(:gossip).permit(:title, :content))
+    @gossip = Gossip.new(gossip_params)
     
     if @gossip.save
       redirect_to @gossip, notice: "Bravo! You created a new message"
@@ -27,13 +27,20 @@ class GossipsController < ApplicationController
   end
 
   def update
-    # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
-    # pour info, le contenu de ce formulaire sera accessible dans le hash params
-    # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
+    if @gossip.update(params[:gossip].permit(:title, :content))
+      redirect_to @gossip
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     # Méthode qui récupère le potin concerné et le détruit en base
     # Une fois la suppression faite, on redirige généralement vers la méthode index (pour afficher la liste à jour)
   end
+
+  private
+    def gossip_params
+      params.require(:gossip).permit(:title, :content)
+    end
 end
